86ce77bc647893065a467201f25f2fbe7aba1177
Crowdbotics <engineering@crowdbotics.com>
2023-08-22 11:32:50 +0000

    Initial commit.

---
+++ backend/augu22_dev_110421/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for augu22_dev_110421 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'augu22_dev_110421.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "augu22-dev-110421",
+  url: "https://augu22-dev-110421.botics.co",
+  api: "https://augu22-dev-110421.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
